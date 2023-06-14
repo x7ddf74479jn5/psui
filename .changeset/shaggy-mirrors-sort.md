e04acd9c6cb91f8e47f29e2fb9b2a7c34eaee7af
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-06-14 19:05:01 +0900

    🔧 config: update build config

---
+++ .changeset/shaggy-mirrors-sort.md
@@ -0,0 +1,5 @@
+---
+"@x7ddf74479jn5/psui": patch
+---
+
+fix build and publish configs
--- packages/components/tsup.config.ts
+++ packages/components/tsup.config.ts
@@ -8,6 +8,7 @@ export default defineConfig((options: Options) => ({
   dts: true,
   minify: true,
   clean: true,
+  sourcemap: true,
   external: ["react"],
   ...options,
 }));
